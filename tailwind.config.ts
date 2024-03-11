03d5efe1ae7192adf5d73788fe6bb2ae30c8279f
Gabriel Rieff <gabrielrieff1@gmail.com>
2024-03-11 20:24:10 -0300

    refactor

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,13 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    screens: {
+      "2xl": { max: "1535px" },
+      xl: { max: "1279px" },
+      lg: { max: "1023px" },
+      md: { max: "827px" },
+      sm: { max: "639px" },
+    },
   },
   plugins: [],
 };
